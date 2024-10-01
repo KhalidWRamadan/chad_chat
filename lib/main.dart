86cc76aff40726e6b90bdec3f52ee210c4a579e3
Khalid Ramadan <khalid.waleed.ra@gmail.com>
2024-10-01 17:54:44 +0300

    initializing firebase for all platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:chad_chat/firebase_options.dart';
 import 'package:chad_chat/views/login_view.dart';
 import 'package:chad_chat/views/sign_up_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChadChat());
 }
 
